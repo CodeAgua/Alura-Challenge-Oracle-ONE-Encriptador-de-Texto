5b43a31167c6031079812aac8ad6d94e841d772b
WaterCode <96397960+CodeAgua@users.noreply.github.com>
2022-05-01 20:28:15 -0300

    Trying to make the crypt logic work

--- js/index.js
+++ js/index.js
@@ -1,42 +1,63 @@
 var textoEncriptable = document.querySelector("#boton-encriptar");
 
-textoEncriptable.addEventListener('click', function(){
+    textoEncriptable.addEventListener('click', function(){
+
     var textarea1 = document.querySelector("#texto-cript");
-    var texto1 = textarea1.value;
     var resultEncriptado = document.querySelector(".criptreemplazo-texto");
-    resultEncriptado.textContent = texto1;
-    console.log(texto1);
-    console.log(typeof texto1);
-    texto1.reset();
+    var texto1 = textarea1.value;
+    resultEncriptado = encriptarTexto(texto1);
+    console.log(resultEncriptado);
+    // texto1.reset();
 });
 
 // function capturarTextarea(textarea) {return texto}
 
 var textoDesencriptable = document.querySelector("#boton-desencriptar");
 
-textoDesencriptable = addEventListener('click', function(){
+    textoDesencriptable = addEventListener('click', function(){
 
     var textarea2 = document.querySelector("#texto-cript");
-    var texto2 = textarea2.value;
     var resultDesencriptado = document.querySelector(".criptreemplazo-texto");
-    resultDesencriptado.textContent = texto2;
-    console.log(texto2);
-    texto2.reset();
+    var texto2 = textarea2.value;
+    resultDesencriptado = desencriptarTexto(texto2);
+    console.log(resultDesencriptado);
+    // texto2.reset();
     });
 
 
- function encriptarTexto(textoEncriptable) {
-    str = str.replace(new RegExp("e", "gmi"), "enter");
-    str = str.replace(new RegExp("i", "gmi"), "imes");
-    str = str.replace(new RegExp("a", "gmi"), "ai");
-    str = str.replace(new RegExp("o", "gmi"), "ober");
-    str = str.replace(new RegExp("u", "gmi"), "ufat");
+/* function encriptarTexto(string) {
+    string = string.replaceAll(new RegExp("e", "gmi"), "enter");
+    string = string.replaceAll(new RegExp("i", "gmi"), "imes");
+    string = string.replaceAll(new RegExp("a", "gmi"), "ai");
+    string = string.replaceAll(new RegExp("o", "gmi"), "ober");
+    string = string.replaceAll(new RegExp("u", "gmi"), "ufat");
+    return;
 }
 
-function desencriptarTexto(textoDesencriptable) {
-    str = str.replace(new RegExp("enter", "gmi"), "e");
-    str = str.replace(new RegExp("imes", "gmi"), "i");
-    str = str.replace(new RegExp("ai", "gmi"), "a");
-    str = str.replace(new RegExp("ober", "gmi"), "o");
-    str = str.replace(new RegExp("ufat", "gmi"), "u");
+// function desencriptarTexto(string) {
+    string = string.replaceAll(new RegExp("enter", "gmi"), "e");
+    string = string.replaceAll(new RegExp("imes", "gmi"), "i");
+    string = string.replaceAll(new RegExp("ai", "gmi"), "a");
+    string = string.replaceAll(new RegExp("ober", "gmi"), "o");
+    string = string.replaceAll(new RegExp("ufat", "gmi"), "u");
+    return;
+}
+*/
+
+function encriptarTexto(string) {
+    string = string.split("e").join("enter");
+    string = string.split("i").join("imes");
+    string = string.split("a").join("ai");
+    string = string.split("o").join("ober");
+    string = string.split("u").join("ufat");
+    return;
+}
+
+function desencriptarTexto(string) {
+    string = string.split("enter").join("e");
+    string = string.split("imes").join("i");
+    string = string.split("ai").join("a");
+    string = string.split("ober").join("o");
+    string = string.split("ufat").join("u");
+    return;
 }
