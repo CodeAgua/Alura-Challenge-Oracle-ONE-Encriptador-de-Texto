27772435d68742cfcb8953a5d8263232cce8be2d
WaterCode <96397960+CodeAgua@users.noreply.github.com>
2022-05-02 04:30:52 -0300

    Trying to make the copy text work

--- js/index.js
+++ js/index.js
@@ -40,5 +40,18 @@ var textoDesencriptable = document.querySelector("#boton-desencriptar");
     resultDesencriptado.textContent = desencriptarTexto(capturarTextarea(textarea));
     });
 
-new ClipboardJS(".btn");
+var clipboard = new ClipboardJS('.btn');
+
+    clipboard.on('success', function(e) {
+        console.info('Action:', e.action);
+        console.info('Text:', e.text);
+        console.info('Trigger:', e.trigger);
+    
+        e.clearSelection();
+    });
+    
+    clipboard.on('error', function(e) {
+        console.error('Action:', e.action);
+        console.error('Trigger:', e.trigger);
+    });
 
